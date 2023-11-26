0daf6541c3cb4e245ba23839e4b6a650f73670c7
andre-azuu <andre.azu@strathmore.edu>
2023-11-27 02:20:52 +0300

    Add orders file

--- opiyo/resources/views/welcome.blade.php
+++ opiyo/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Opiyo;s restaurant</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
