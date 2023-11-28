ef5008b93cd65ecdabfb3612d2e9542138dde618
andre-azuu <andre.azu@strathmore.edu>
2023-11-28 23:19:45 +0300

    final version 1

--- opiyo/resources/views/posts/edit.blade.php
+++ opiyo/resources/views/posts/edit.blade.php
@@ -8,23 +8,263 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-    <title>Edit Post</title>
+    <title>Edit recipe</title>
+
+    <style>
+        /* 64ac15 */
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+body {
+  padding: 1em;
+  font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 15px;
+  color: #b9b9b9;
+  background-size: 99%;
+  
+    background-image: url('https://cravinghomecooked.com/wp-content/uploads/2022/12/pesto-chicken-and-veggies-1-14-650x650.jpg');
+    background-repeat: no-repeat;
+  margin-left: max(500px, ((40% - 260px) / 2));
+  font: 90%/1.4 system-ui;
+}
+
+.card {
+  position: relative;
+  top: 0;
+  background: white;
+  padding: 1.5rem;
+  box-shadow: 0 -0.5rem 1rem rgba(0 0 0 / 0.15);
+  border-radius: 40px;
+}
+
+
+
+h4 {
+  color: #f0a500;
+}
+input,
+input[type="radio"] + label,
+input[type="checkbox"] + label:before,
+select option,
+select {
+  width: 100%;
+  padding: 1em;
+  line-height: 1.4;
+  background-color: #f9f9f9;
+  border: 1px solid #e5e5e5;
+  border-radius: 3px;
+  -webkit-transition: 0.35s ease-in-out;
+  -moz-transition: 0.35s ease-in-out;
+  -o-transition: 0.35s ease-in-out;
+  transition: 0.35s ease-in-out;
+  transition: all 0.35s ease-in-out;
+}
+input:focus {
+  outline: 0;
+  border-color: #bd8200;
+}
+input:focus + .input-icon i {
+  color: #f0a500;
+}
+input:focus + .input-icon:after {
+  border-right-color: #f0a500;
+}
+input[type="radio"] {
+  display: none;
+}
+input[type="radio"] + label,
+select {
+  display: inline-block;
+  width: 50%;
+  text-align: center;
+  float: left;
+  border-radius: 0;
+}
+input[type="radio"] + label:first-of-type {
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+input[type="radio"] + label:last-of-type {
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+}
+input[type="radio"] + label i {
+  padding-right: 0.4em;
+}
+input[type="radio"]:checked + label,
+input:checked + label:before,
+select:focus,
+select:active {
+  background-color: #f0a500;
+  color: #fff;
+  border-color: #bd8200;
+}
+input[type="checkbox"] {
+  display: none;
+}
+input[type="checkbox"] + label {
+  position: relative;
+  display: block;
+  padding-left: 1.6em;
+}
+input[type="checkbox"] + label:before {
+  position: absolute;
+  top: 0.2em;
+  left: 0;
+  display: block;
+  width: 1em;
+  height: 1em;
+  padding: 0;
+  content: "";
+}
+input[type="checkbox"] + label:after {
+  position: absolute;
+  top: 0.45em;
+  left: 0.2em;
+  font-size: 0.8em;
+  color: #fff;
+  opacity: 0;
+  font-family: FontAwesome;
+  content: "\f00c";
+}
+input:checked + label:after {
+  opacity: 1;
+}
+select {
+  height: 3.4em;
+  line-height: 2;
+}
+select:first-of-type {
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+select:last-of-type {
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+}
+select:focus,
+select:active {
+  outline: 0;
+}
+select option {
+  background-color: #f0a500;
+  color: #fff;
+}
+.input-group {
+  margin-bottom: 1em;
+  zoom: 1;
+}
+.input-group:before,
+.input-group:after {
+  content: "";
+  display: table;
+}
+.input-group:after {
+  clear: both;
+}
+.input-group-icon {
+  position: relative;
+}
+.input-group-icon input {
+  padding-left: 4.4em;
+}
+.input-group-icon .input-icon {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 3.4em;
+  height: 3.4em;
+  line-height: 3.4em;
+  text-align: center;
+  pointer-events: none;
+}
+.input-group-icon .input-icon:after {
+  position: absolute;
+  top: 0.6em;
+  bottom: 0.6em;
+  left: 3.4em;
+  display: block;
+  border-right: 1px solid #e5e5e5;
+  content: "";
+  -webkit-transition: 0.35s ease-in-out;
+  -moz-transition: 0.35s ease-in-out;
+  -o-transition: 0.35s ease-in-out;
+  transition: 0.35s ease-in-out;
+  transition: all 0.35s ease-in-out;
+}
+.input-group-icon .input-icon i {
+  -webkit-transition: 0.35s ease-in-out;
+  -moz-transition: 0.35s ease-in-out;
+  -o-transition: 0.35s ease-in-out;
+  transition: 0.35s ease-in-out;
+  transition: all 0.35s ease-in-out;
+}
+.container {
+  max-width: 38em;
+  padding: 1em 3em 2em 3em;
+  margin: 0em auto;
+  background-color: #fff;
+  border-radius: 4.2px;
+  box-shadow: 0px 3px 10px -2px rgba(0, 0, 0, 0.2);
+}
+.row {
+  zoom: 1;
+}
+.row:before,
+.row:after {
+  content: "";
+  display: table;
+}
+.row:after {
+  clear: both;
+}
+.col-half {
+  padding-right: 10px;
+  float: left;
+  width: 50%;
+}
+.col-half:last-of-type {
+  padding-right: 0;
+}
+.col-third {
+  padding-right: 10px;
+  float: left;
+  width: 33.33333333%;
+}
+.col-third:last-of-type {
+  padding-right: 0;
+}
+@media only screen and (max-width: 540px) {
+  .col-half {
+    width: 100%;
+    padding-right: 0;
+  }
+}
+
+    </style>
+
+
+
+
+
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+<nav class="navbar navbar-expand-lg navbar-light bg-warning" style="border-radius: 20px">
         <div class="container-fluid">
-            <a class="navbar-brand h1" href={{ route('posts.index') }}>CRUDPosts</a>
+            <a class="navbar-brand h1" href={{ route('posts.index') }}>Change the recipe</a>
             <div class="justify-end ">
                 <div class="col ">
-                    <a class="btn btn-sm btn-success" href={{ route('posts.create') }}>Add Post</a>    
+                    <a class="btn btn-sm btn-success" href="/">Home</a>
                 </div>
             </div>
     </nav>
     <div class="container h-100 mt-5">
         <div class="row h-100 justify-content-center align-items-center">
             <div class="col-10 col-md-8 col-lg-6">
-                <h3>Update Post</h3>
+                <h3>Edit recipe</h3>
                 <form action="{{ route('posts.update', $post->id) }}" method="post">
                     @csrf
                     @method('PUT')
@@ -37,7 +277,9 @@
                         <label for="body">Body</label>
                         <textarea class="form-control" id="body" name="body" rows="3" required>{{ $post->body }}</textarea>
                     </div>
-                    <button type="submit" class="btn btn-primary">Update Post</button>
+
+                    <br>
+                    <button type="submit" class="btn btn-primary" style="background-color: #f0a500;">Update Post</button>
                 </form>
             </div>
         </div>
--- opiyo/resources/views/posts/index.blade.php
+++ opiyo/resources/views/posts/index.blade.php
@@ -6,25 +6,59 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
 integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <title>Posts</title>
+  <title>Opiyo's Kitchen</title>
+  <link rel="icon" href="https://png.pngtree.com/png-vector/20220527/ourmid/pngtree-food-logo-png-image_4743675.png" type="image/icon type">
+<style>
+  body{
+    background-image: url('https://www.gohomely.com/assets/img/web/new_home_hero_img.png');
+    background-repeat: repeat-y;
+  margin-left: max(500px, ((40% - 260px) / 2));
+  font: 90%/1.4 system-ui;
+}
+
+.card {
+  position: relative;
+  top: 0;
+  background: white;
+  padding: 1.5rem;
+  box-shadow: 0 -0.5rem 1rem rgba(0 0 0 / 0.15);
+  border-radius: 40px;
+}
+
+
+
+
+
+
+
+  </style>
+
+
+
+
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+  <nav class="navbar navbar-expand-lg navbar-light bg-warning" style="border-radius: 20px;
+  padding: 5px;">
     <div class="container-fluid">
-      <a class="navbar-brand h1" href="{{ route('posts.index') }}">CRUDPosts</a>
+      
+      <a class="navbar-brand h1" href="{{ route('posts.index') }}">Opiyo's Kitchen</a>
       <div class="justify-end ">
         <div class="col ">
-          <a class="btn btn-sm btn-success" href= "{{ route('posts.create') }}">Add Post</a>
+          <a class="btn btn-sm btn-success" href= "{{ route('posts.create') }}">Add personal recipe</a>
         </div>
       </div>
     </div>
   </nav>
+  <div>
+  
+  </div>
   <div class="container mt-5">
     <div class="row">
       @foreach ($posts as $post)
         <div class="col-sm">
           <div class="card">
-            <div class="card-header">
+            <div class="card-header" style="color: #f0a500; border: 30px;">
               <h5 class="card-title">{{ $post->title }}</h5>
             </div>
             <div class="card-body">
@@ -34,13 +68,15 @@ integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6
               <div class="row">
                 <div class="col-sm">
                   <a href="{{ route('posts.edit', $post->id) }}"
-            class="btn btn-primary btn-sm">Edit</a>
+            class="btn sm" style= "background-color:#f0a500; color:white;" >Edit</a>
+            <br>
+            <br>
                 </div>
                 <div class="col-sm">
                     <form action="{{ route('posts.destroy', $post->id) }}" method="post">
                       @csrf
                       @method('DELETE')
-                      <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                      <button type="submit" class="btn btn-danger btn-sm">Delete </button>
                     </form>
                 </div>
               </div>
